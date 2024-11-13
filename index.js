c5a8db7a210a683ae5e5fd340dd65f7e7a732d2b
MukarramHaq <mukarram796@gmail.com>
2024-11-13 16:06:47 -0500

    Mobile Desktop done and the accordion is 100% accessible

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-const plusIconEl = document.querySelector(".plus-icon")
-const minusIconEl = document.querySelector(".minus-icon")
+const minusIcons = document.querySelectorAll(".minus-icon");
+const plusIcons = document.querySelectorAll(".plus-icon");
+const answerContainers = document.querySelectorAll('.answer-container');
+const questionContainers = document.querySelectorAll('.question-container');
 
-minusIconEl.addEventListener("click", () => {
-    minusIconEl.style.display = "none";
-    plusIconEl.style.display = "block";
+// for(let i = 0; i < answerContainers.length; i++){
+//     answerContainers[i].style.maxHeight = 0;
+//     answerContainers[i].style.overflow = 'hidden';
+// }
 
-    
-
+minusIcons.forEach((minusIcon, index) => {
+  minusIcon.addEventListener("click", () => {
+    minusIcon.style.display = "none"; // Hide the minus icon
+    plusIcons[index].style.display = "block"; // Show the corresponding plus icon
+    answerContainers[index].style.maxHeight = 0;
+    answerContainers[index].style.overflow = 'hidden';
+    questionContainers[index].style.marginBottom = 0;
+  });
 });
 
-plusIconEl.addEventListener("click", () => {
-    minusIconEl.style.display = "block";
-    plusIconEl.style.display = "none";
+plusIcons.forEach((plusIcon, index) => {
+  plusIcon.addEventListener("click", () => {
+    plusIcon.style.display = "none"; // Hide the minus icon
+    minusIcons[index].style.display = "block"; // Show the corresponding plus icon
+    answerContainers[index].style.maxHeight = 'none';
+    questionContainers[index].style.marginBottom = '30px';
+  });
 });
-
-console.log(plusIconEl);
-console.log(minusIconEl);
